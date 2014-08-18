c18ce251b6dc40f02870f43cf5687f6f229f9b46
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2014-08-18 21:48:46 +0200

    Image with integer vs floating point color channels

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include "CImg.h"
 using namespace cimg_library;
 
-int main() {
-   CImg<unsigned char> img(10, 10, 1, 3);  // Define a 640x400 color image with 8 bits per color component.
-   img.fill(0);                            // Set pixel values to 0 (color : black)
-   unsigned char purple[] = {255, 0, 255};         // Define a purple color
-// img.draw_text(10, 10, "Hello World", purple);   // Draw a purple "Hello world" at coordinates (100,100).
-   img.draw_point(0, 0, purple);
-   img.draw_line(1, 0, 1, 9, purple);
-   img.display("hello");             // Display the image in a display window.
-// img.save("hello.png");
+#include <cstdint>
 
-   return 0;
+int main()
+{
+    // Integer color image (standard 2D image/file format)
+    CImg<uint8_t> img(32, 4, 1, 3);         // Define a 32x4 color image with 8 bits per color component.
+    img.fill(0);                            // Set pixel values to 0 (color : black)
+    uint8_t purple[] = {255, 0, 255};       // Define a purple color
+//  img.draw_text(10, 10, "Hello World", purple);
+    img.draw_point(0, 0, purple);
+    img.draw_line(2, 0, 2, 3, purple);
+    img.display("hello img");               // Display the image in a display window.
+//  img.save("hello.png");
+
+    // Floating point color image (standard 3D color/texture management)
+    CImg<float> imgf(32, 4, 1, 3);          // Define a 32x4 color image with 8 bits per color component.
+    imgf.fill(0.f);                         // Set pixel values to 0 (color : black)
+    float purplef[] = { 1.f, 0.f, 1.f };    // Define a purple color
+    imgf.draw_point(0, 0, purple);
+    imgf.draw_line(2, 0, 2, 3, purple);
+    imgf.display("hello imgf");             // Display the image in a display window.
+
+    return 0;
 }
