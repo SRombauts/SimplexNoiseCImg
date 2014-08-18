529ab9883d82460a75397b37310edc79efbd38ba
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2014-08-18 22:05:14 +0200

    Displays a linear gradient color

--- main.cpp
+++ main.cpp
@@ -5,23 +5,35 @@ using namespace cimg_library;
 
 int main()
 {
+/*
     // Integer color image (standard 2D image/file format)
-    CImg<uint8_t> img(32, 4, 1, 3);         // Define a 32x4 color image with 8 bits per color component.
-    img.fill(0);                            // Set pixel values to 0 (color : black)
-    uint8_t purple[] = {255, 0, 255};       // Define a purple color
+    CImg<uint8_t> img(32, 4, 1, 3);     // Define a 32x4 color image with 8 bits per color component.
+    img.fill(0);                        // Set pixel values to 0 (color : black)
+    uint8_t purple[] = {255, 0, 255};   // Define a purple color
 //  img.draw_text(10, 10, "Hello World", purple);
     img.draw_point(0, 0, purple);
     img.draw_line(2, 0, 2, 3, purple);
-    img.display("hello img");               // Display the image in a display window.
+    img.display("hello img");           // Display the image in a display window.
 //  img.save("hello.png");
 
     // Floating point color image (standard 3D color/texture management)
-    CImg<float> imgf(32, 4, 1, 3);          // Define a 32x4 color image with 8 bits per color component.
-    imgf.fill(0.f);                         // Set pixel values to 0 (color : black)
-    float purplef[] = { 1.f, 0.f, 1.f };    // Define a purple color
+    CImg<float> imgf(32, 4, 1, 3);      // Define a 32x4 color image with 8 bits per color component.
+    imgf.fill(0.f);                     // Set pixel values to 0 (color : black)
+    float purplef[] = {1.f, 0.f, 1.f};  // Define a purple color
     imgf.draw_point(0, 0, purple);
     imgf.draw_line(2, 0, 2, 3, purple);
-    imgf.display("hello imgf");             // Display the image in a display window.
+    imgf.display("hello imgf");         // Display the image in a display window.
+*/
+
+    CImg<float> img(32, 4, 1, 3);       // Define a 32x4 color image with 8 bits per color component.
+    img.fill(0.f);                      // Set pixel values to 0 (color : black)
+    for (int col = 0; col < img.width(); ++col)
+    {
+        float x = (float)col / img.width();
+        float color[] = {x, x, x};  // Define the color
+        img.draw_line(col, 0, col, 3, color);
+    }
+    img.display("noise");               // Display the image in a display window.
 
     return 0;
 }
