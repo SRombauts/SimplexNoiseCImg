d5b48790770c42c9d7884c1383884843bf01ac25
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-03-16 22:02:15 +0100

    Add lower frequency "continents" (needs more work)

--- main.cpp
+++ main.cpp
@@ -31,50 +31,39 @@ private:
 
 int main() {
     float scale     = 400.f;
-    float offset_x  = 0.0f;
-    float offset_y  = 0.0f;
-    float offset_z  = 0.0f;
-    float lacunarity = 2.0f;
-    float persistance = 0.5f;
-/*
-    float lacunarity = 1.55f;
-    float persistance = 0.65f;
-*/    
-    // TODO NOCOMMIT :
-    float min;
-    float max;
+    float offset_x  = 2.5f;
+    float offset_y  = 4.5f;
+    float offset_z  = 0.05f;
+    float lacunarity    = 1.9f;
+    float persistance   = 0.5f;
 
     Img img;
 
     while (!img.is_closed()) {
         Measure measure;
         measure.start();
-        const SimplexNoise simplex(1.0f/scale, 0.5f, lacunarity, persistance);   // Amplitude of 0.5 for the 1st octave : sum ~1.0f
+        const SimplexNoise continents(0.2f/scale, 0.33f, 1.55f, 0.65f);
+        const SimplexNoise details(1.0f/scale, 0.2f, lacunarity, persistance); // Amplitude of 0.5 for the 1st octave : sum ~1.0f
         const int octaves = static_cast<int>(2 + std::log(scale)); // Estimate number of octaves needed for the current scale
         std::ostringstream title;
-        title << "2D Simplex Perlin noise (" << octaves << " octaves)";
+        title << "2D Simplex Perlin noise (" << 3+octaves << " octaves)";
         img.set_title(title.str().c_str());
         for (int row = 0; row < img.height(); ++row) {
             const float y = static_cast<float>(row - img.height()/2 + offset_y*scale);
             for (int col = 0; col < img.width(); ++col) {
                 const float x = static_cast<float>(col - img.width()/2 + offset_x*scale);
                 
-                // TODO(SRombauts): Generate "biomes", ie smooth geographic variation in frequency & amplitude 
-                // TODO(SRombauts): Add 'erosion' with simple smoothing like exponential
-            //  const float biome = SimplexNoise::noise(x, y);
-            //  const float noise = 0.5f * biome + 0.5f * simplex.fractal(octaves, x, y) + offset_z;
-                // Get the noise value for the coordinate
-                const float noise = simplex.fractal(octaves, x, y) + offset_z;
-                min = std::min(min, noise);
-                max = std::min(max, noise);
+                // TODO(SRombauts): Add 'erosion' with simple smoothing like exponential, and other 'modifiers' like in libnoise
+                // Generate "biomes", ie smooth geographic variation in frequency & amplitude, 
+                // and add smaller details, summing the noise values for the coordinate
+                const float noise = continents.fractal(2, x, y) + details.fractal(octaves, x, y) + offset_z;
                 const color3f color = ramp(noise); // convert to color
                 img.draw_point(col, row, (float*)&color);
             }
         }
         img.display();
         const double diff_ms = measure.get();
-        std::cout << std::fixed << diff_ms << "ms - ";
-        std::cout << "minmax[" << min << "," << max << "]\n";
+        std::cout << std::fixed << diff_ms << "ms\n";
 
         img.user(scale, offset_x, offset_y, offset_z, lacunarity, persistance);
     }
