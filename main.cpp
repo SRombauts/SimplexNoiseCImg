546a6d0be925956f2ae1258a6075a29a606997b7
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-03-18 13:46:11 +0100

    Revert "biomes" : was not the right way to do it

--- main.cpp
+++ main.cpp
@@ -31,10 +31,10 @@ private:
 
 int main() {
     float scale     = 400.f;
-    float offset_x  = 2.5f;
-    float offset_y  = 4.5f;
+    float offset_x  = 5.9f;
+    float offset_y  = 5.1f;
     float offset_z  = 0.05f;
-    float lacunarity    = 1.9f;
+    float lacunarity    = 1.99f;
     float persistance   = 0.5f;
 
     Img img;
@@ -42,11 +42,10 @@ int main() {
     while (!img.is_closed()) {
         Measure measure;
         measure.start();
-        const SimplexNoise continents(0.2f/scale, 0.33f, 1.55f, 0.65f);
-        const SimplexNoise details(1.0f/scale, 0.2f, lacunarity, persistance); // Amplitude of 0.5 for the 1st octave : sum ~1.0f
-        const int octaves = static_cast<int>(2 + std::log(scale)); // Estimate number of octaves needed for the current scale
+        const SimplexNoise simplex(0.1f/scale, 0.5f, lacunarity, persistance); // Amplitude of 0.5 for the 1st octave : sum ~1.0f
+        const int octaves = static_cast<int>(5 + std::log(scale)); // Estimate number of octaves needed for the current scale
         std::ostringstream title;
-        title << "2D Simplex Perlin noise (" << 3+octaves << " octaves)";
+        title << "2D Simplex Perlin noise (" << octaves << " octaves)";
         img.set_title(title.str().c_str());
         for (int row = 0; row < img.height(); ++row) {
             const float y = static_cast<float>(row - img.height()/2 + offset_y*scale);
@@ -56,7 +55,7 @@ int main() {
                 // TODO(SRombauts): Add 'erosion' with simple smoothing like exponential, and other 'modifiers' like in libnoise
                 // Generate "biomes", ie smooth geographic variation in frequency & amplitude, 
                 // and add smaller details, summing the noise values for the coordinate
-                const float noise = continents.fractal(2, x, y) + details.fractal(octaves, x, y) + offset_z;
+                const float noise = simplex.fractal(octaves, x, y) + offset_z;
                 const color3f color = ramp(noise); // convert to color
                 img.draw_point(col, row, (float*)&color);
             }
